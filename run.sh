bb005d74b21666e02297f4b0aa97f29a03abd76c
Aqila Nafsah <164625138+aqilanafsah@users.noreply.github.com>
2024-03-23 17:18:04 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-6695af6d-8b5f-4e93-9a1e-538def012b2d/)
 
 
 echo "$curl_output" >> Update.tsx
